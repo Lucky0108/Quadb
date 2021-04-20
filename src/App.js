5ff4903735da610809218353f13cf18fb4ee4f3a
Lucky0108 <raolakshayyadav02@gmail.com>
2021-04-20 22:14:47 +0530

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
 
 function App() {
   return (
+    <>
     <div className="App">
-     
+     <Home />
     </div>
+      <div className="add-to-home-div">
+      <button className="btn btn-outline-info">Add hodlinfo to home screen</button>
+    </div>
+  </>
   );
 }
 
